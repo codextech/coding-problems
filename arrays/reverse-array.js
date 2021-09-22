f60c96443a42e145e09fcf7899fa50eb3493062a
Tanzeel <tanzeelsaleem10@gmail.com>
2021-09-23 00:11:58 +0500

    solve "reverse array" coding problem

---
+++ arrays/reverse-array.js
@@ -0,0 +1,38 @@
+
+
+// Write a program to reverse an array;
+
+/* 
+Input :  arr = [4, 5, 1, 2]
+Output : arr = [2, 1, 5, 4]
+*/
+
+/* ================= SOLUTION ================= */
+
+// we have to loop through each elemnt to change the position of element
+/* >>>> TIME COMPLEXISTY : O(n) <<<< */
+
+function reverseArray(arr) {
+  
+    let arrayStart = 0; 
+    let arrayEnd = arr.length-1;
+
+    /* read it from both side and swap the element */
+
+    while(arrayStart < arrayEnd) {
+        let temp = arr[arrayEnd];
+        /* swap */
+        arr[arrayEnd] = arr[arrayStart];
+        arr[arrayStart] = temp;
+        /* incriment to read next element */
+        arrayStart++;
+        /* decriment to read previous element */
+        arrayEnd--;
+    }
+
+    return arr;
+}
+
+// call function
+const result  = reverseArray([4, 5, 1, 2])
+console.log(result);
